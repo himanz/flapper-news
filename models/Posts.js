7b617b92b400c0426bdb143a0fe07eb9fb2db4e1
Jonathan Chang <jonoschang7@gmail.com>
2014-09-12 02:33:06 -0400

    create model for posts

---
+++ models/Posts.js
@@ -0,0 +1,8 @@
+var PostSchema = new mongoose.Schema({
+  title: String,
+  link: String,
+  upvotes: {type: Number, default: 0},
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+});
+
+mongoose.model('Post', PostSchema);
